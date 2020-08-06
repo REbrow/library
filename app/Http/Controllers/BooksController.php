319ca46a2c31272a3c67a719f21be2a75903ded0
David Olaoye <dolaoye3@gmail.com>
2020-08-06 03:50:43 +0100

    Book Resource

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    //
+    public function store(){
+
+        $data = $this->validateRequest();
+        Book::create($data);
+    }
+
+    public function update(Book $book) {
+        $data = $this->validateRequest();
+        $book->update($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required'
+        ]);
+    }
+}
